aa54d36c8c8b3debedd4d5e8b4192ab216b6199c
Roger Martins da silva <roger.8b@gmail.com>
2018-08-16 01:13:55 -0300

    Adicionado envio da mensagem para o banco

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -117,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: Send messages on click
 
                 FriendlyMessage friendlyMessage = new FriendlyMessage(mMessageEditText.getText().toString(), mUsername, null);
+                mMessagesDatabaseReference.push().setValue(friendlyMessage);
 
                 // Clear input box
                 mMessageEditText.setText("");
